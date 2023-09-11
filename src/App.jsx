5f4123271928a66ee4b2204f706f0ca9fb0e2a8c
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-09-11 17:34:46 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
